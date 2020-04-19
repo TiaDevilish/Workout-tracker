2e14be6000bbbce7deea48955280aef47565dae0
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-04-19 17:57:27 -0400

    added api routes to server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 7500;
+
+const db = require("./models")
 
 const app = express();
 
@@ -12,15 +15,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser:true,
-    useFindAndModify: false
+//api
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"./public/index.html"))
 });
 
-const db = require("./models")
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
+});
 
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+app.get("exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
+});
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser:true
+});
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}!`)
